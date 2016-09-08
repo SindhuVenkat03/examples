aeaf471b726b473ebe07567cdae080b422116735
SindhuVenkat03 <lakshmisriiyer@gmail.com>
2016-09-08 12:01:36 +0530

    Add files via upload

---
+++ Reverseguvi.java
@@ -0,0 +1,12 @@
+package MS3.LogicalExcercise;
+
+public class Reverseguvi
+{
+	public static void main(String[] args) 
+	{
+		String s1 = "sindhu";
+		StringBuffer s2 = new StringBuffer(s1);
+		StringBuffer s3 =s2.reverse();
+		System.out.println(s3);
+}
+}
