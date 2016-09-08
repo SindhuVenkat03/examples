a4f461c75276510dbf468c5ca59115ae53dfbd0c
SindhuVenkat03 <lakshmisriiyer@gmail.com>
2016-09-08 12:31:21 +0530

    Add files via upload

---
+++ Reverseintguvi.java
@@ -0,0 +1,16 @@
+package MS3.LogicalExcercise;
+
+public class Reverseintguvi 
+{
+	public static void main(String[] args) 
+	{
+		
+	String s1 ="12345";
+	StringBuffer sb = new StringBuffer(s1);
+	StringBuffer s2 = sb.reverse();
+	
+	System.out.println(s2);
+	
+	
+}
+}
