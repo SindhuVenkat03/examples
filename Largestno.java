7e8c47ffb507d13d213974c52f1a2f68d598768b
SindhuVenkat03 <lakshmisriiyer@gmail.com>
2016-09-09 15:26:34 +0530

    Add files via upload

---
+++ Largestno.java
@@ -0,0 +1,25 @@
+package MS3.LogicalExcercise;
+
+import java.util.Scanner;
+
+public class Largestno
+{
+	public static void main(String[] args) 
+	{ 
+		int a =50;
+		int b=15;
+		int c=300;
+		if(a>b && b>c)
+		{
+			System.out.println("a is greater");
+		}
+		else if(a>c)
+		{
+			System.out.println("b is great");
+		}
+		else
+		{
+			System.out.println("c is great");
+		}
+	}
+}
